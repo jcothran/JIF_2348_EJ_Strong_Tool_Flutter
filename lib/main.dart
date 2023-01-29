3be8a1d2d95ac0f82bd82ba9ce080b642d75f0c6
Jacob M. Smith <jmsmith6101@att.net>
2023-01-29 17:09:13 -0500

    main now links to the home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:makepdfs/pages/hazardTs.dart';
+// import 'package:makepdfs/pages/hazardTs.dart';
+import 'package:makepdfs/pages/home.dart';
 
 void main() {
   runApp(const MyApp());
@@ -25,7 +26,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: FormsPage(),
+      home: HomePage(),
     );
   }
 }
