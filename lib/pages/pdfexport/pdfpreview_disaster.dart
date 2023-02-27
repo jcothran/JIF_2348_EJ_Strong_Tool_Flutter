94a5efc4751d4e9d0c95d42a36a704ba1c2baa91
max <maxhipp@gmail.com>
2023-02-26 22:53:30 -0500

    Start at Disaster detail PDF preview page

---
+++ lib/pages/pdfexport/pdfpreview_disaster.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:makepdfs/models/disasterT.dart';
+import 'package:makepdfs/pages/pdfexport/pdf/pdfexportDisast.dart';
+import 'package:printing/printing.dart';
+
+
+class PdfPreviewDisasterPage extends StatelessWidget {
+  final DisasterT disasterT;
+  const PdfPreviewDisasterPage({Key? key, required this.disasterT}) : super(key: key);
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('PDF Preview'),
+      ),
+      body: PdfPreview(build: (context) => makePdfDisaster(disasterT),
+      ),
+    );
+  }
+
+
+
+
+}
