53e780b1bf2485750dc5e431feb7f778d0df3611
Jacob M. Smith <jmsmith6101@att.net>
2023-04-29 18:55:40 -0400

    page for editing forms

---
+++ lib/pages/edit_forms.dart
@@ -0,0 +1,194 @@
+import 'package:flutter/material.dart';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:makepdfs/services/auth.dart';
+import 'package:flutter/material.dart';
+import 'package:makepdfs/models/hazardT.dart';
+import 'package:makepdfs/models/vulnerableT.dart';
+import 'package:makepdfs/models/capacityT.dart';
+import 'package:makepdfs/models/disasterT.dart';
+import 'package:makepdfs/pages/hazard_detail.dart';
+import 'package:makepdfs/pages/location_date.dart';
+import 'package:makepdfs/pages/vulnerability_detail.dart';
+import 'package:makepdfs/pages/capacity_detail.dart';
+import 'package:makepdfs/pages/disaster_detail.dart';
+import '../services/database.dart';
+
+//page for navigating to the various form pages
+
+class EditFormsPage extends StatelessWidget {
+  final ButtonStyle style = ElevatedButton.styleFrom(textStyle: const TextStyle(fontSize: 20), fixedSize: Size(240, 80));
+  final TextStyle text_style = TextStyle(fontSize: 20, fontWeight: FontWeight.bold);
+  String locString = LocationDatePage().getLocationString();
+  String loc = LocationDatePage().getLocation();
+  String date = LocationDatePage().getDate();
+  String location_date = LocationDatePage().getLocation() + " " + LocationDatePage().getDate() + " ";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Edit Forms'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>
+          [
+            Container
+              (
+              child: Text
+                (
+                locString + '\n' + date,
+                style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            IconButton(
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => LocationDatePage())
+                );
+              },
+              icon: Icon(
+                Icons.add_location_alt,
+                size: 40.0,
+              ),
+            ),
+            Container
+              (
+              child: Text
+                (
+                "Update Loc/Date",
+                style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(15),
+            ),
+            Container
+              (
+              child: Text
+                (
+                "Select a type of form",
+                style: text_style,
+              ),
+            ),
+            Container(
+              child: Divider(),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 8),
+              child: ElevatedButton(
+                child: Text(
+                  "Hazard Assessment Form",
+                ),
+                style: style,
+                onPressed:() async
+                {
+                  FirebaseFirestore db = FirebaseFirestore.instance;
+                  String uid = AuthService().getUserId().toString();
+                  DocumentReference data = db.collection("hazard_form").doc(location_date + uid);
+                  await data.get().then(
+                        (dataSnapshot) => {
+                      if (dataSnapshot.exists) {
+                        print(dataSnapshot.data()),
+                        // print(dataSnapshot.get('desc')),
+                        Navigator.of(context).push(MaterialPageRoute(builder: (context) =>HazardDetailPage(hazardT: HazardT.convertHazardDocument(dataSnapshot), edit_file: true,)))
+                      },
+                    },
+                    onError: (e) => print("Error completing: $e"),
+                  );
+                },
+              ),
+            ),
+            Container(
+              child: Divider(),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 8),
+              child: ElevatedButton(
+                child: Text(
+                  "Vulnerability Assessment Form",
+                ),
+                style: style,
+                onPressed:() async
+                {
+                  FirebaseFirestore db = FirebaseFirestore.instance;
+                  String uid = AuthService().getUserId().toString();
+                  DocumentReference data = db.collection("vulnerability_form").doc(location_date + uid);
+                  await data.get().then(
+                        (dataSnapshot) => {
+                      if (!dataSnapshot.exists) {
+                        DatabaseService().addVulnerableData(location_date + uid),
+                      }
+                    },
+                    onError: (e) => print("Error completing: $e"),
+                  );
+                  // Navigator.of(context).push(MaterialPageRoute(builder: (context)=> VulnerabilityDetailPage(vulnerableT: blankV)));
+                },
+              ),
+            ),
+            Container(
+              child: Divider(),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 8),
+              child: ElevatedButton(
+                child: Text(
+                  "Capacity Assessment Form",
+                ),
+                style: style,
+                onPressed:() async
+                {
+                  FirebaseFirestore db = FirebaseFirestore.instance;
+                  String uid = AuthService().getUserId().toString();
+                  DocumentReference data = db.collection("capacity_form").doc(location_date + uid);
+                  await data.get().then(
+                        (dataSnapshot) => {
+                      if (!dataSnapshot.exists) {
+                        print('Data exists'),
+                        DatabaseService().addCapacityData(location_date + uid),
+                      }
+                    },
+                    onError: (e) => print("Error completing: $e"),
+                  );
+                  // Navigator.of(context).push(MaterialPageRoute(builder: (context)=> CapacityDetailPage(capacityT: blankC)));
+                },
+              ),
+            ),
+            Container(
+              child: Divider(),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 8),
+              child: ElevatedButton(
+                child: Text("Disaster Risk Analysis Form"),
+                style: style,
+                onPressed:() async
+                {
+                  FirebaseFirestore db = FirebaseFirestore.instance;
+                  String uid = AuthService().getUserId().toString();
+                  DocumentReference data = db.collection("disaster_form").doc(location_date + uid);
+                  await data.get().then(
+                        (dataSnapshot) => {
+                      if (!dataSnapshot.exists) {
+                        DatabaseService().addDisasterData(location_date + uid),
+                      }
+                    },
+                    onError: (e) => print("Error completing: $e"),
+                  );
+                  // Navigator.of(context).push(MaterialPageRoute(builder: (context)=>DisasterDetailPage(disasterT: blankD)));
+                },
+              ),
+            ),
+            Container(
+              child: Divider(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
