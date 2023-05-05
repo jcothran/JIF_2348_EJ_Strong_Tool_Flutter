4d72ee426922ba7c73e1c4e2908656576c8a6a83
Alana McCrary <macalanita@gmail.com>
2023-05-05 00:56:28 -0400

    Fixed the format to fit and adjust to the margins of different screen sizes

--- lib/pages/location_date.dart
+++ lib/pages/location_date.dart
@@ -76,21 +76,26 @@ class LocationDatePageState extends State<LocationDatePage>{
         body: Center(
             child: SingleChildScrollView(
               child: SizedBox(
-                height: 625,
+                height: MediaQuery.of(context).size.height/1.16,
                 child: Column(
                   children: [
                     Spacer(),
-                    const Image(
-                      image: AssetImage('assets/world.png'),
-                      height: 300,
-                      width: 300,
+                    Expanded(
+                      flex: 6,
+                      child: Container(
+                        child: const Image(
+                          image: AssetImage('assets/world.png'),
+                        ),
+                      ),
                     ),
-                    Padding(padding: EdgeInsets.all(17.0)),
-                    Align(
-                      alignment: Alignment(-.70, 0),
-                      child: Text(
-                        'Location:',
-                        style: TextStyle(fontSize: 21),
+                    Padding(padding: EdgeInsets.all(14.0)),
+                    Expanded(
+                      child: Align(
+                        alignment: Alignment(-.70, 0),
+                        child: Text(
+                          'Location:',
+                          style: TextStyle(fontSize: 21),
+                        ),
                       ),
                     ),
                     Padding(padding: EdgeInsets.all(6.0)),
@@ -98,9 +103,9 @@ class LocationDatePageState extends State<LocationDatePage>{
                       child: Row(
                           mainAxisAlignment: MainAxisAlignment.center,
                           children: [
-                          SizedBox(
-                            width: 52,
-                            height: 110,
+                          Container(
+                            height:MediaQuery.of(context).size.height/6.5,
+                            width: MediaQuery.of(context).size.width/6.7 ,
                             child: TextField(
                               maxLength: 2,
                               controller: stateController,
@@ -118,9 +123,9 @@ class LocationDatePageState extends State<LocationDatePage>{
                             ),
                           ),
                           Padding(padding: EdgeInsets.all(7.0)),
-                          SizedBox(
-                            width: 135,
-                            height: 110,
+                          Container(
+                             height:MediaQuery.of(context).size.height/2.8,
+                             width: MediaQuery.of(context).size.width/2.8 ,
                             child: TextField(
                               controller: countyController,
                               textAlign: TextAlign.center,
@@ -139,121 +144,127 @@ class LocationDatePageState extends State<LocationDatePage>{
                         ],
                       ),
                     ),
-                    // Padding(padding: EdgeInsets.all(15.0)),
-                    Align(
-                      alignment: Alignment(-.72, 0),
-                      child: Text(
-                        'Date:',
-                        style: TextStyle(fontSize: 21),
+                    Expanded(
+                      child: Align(
+                        alignment: Alignment(-.72, 0),
+                        child: Text(
+                          'Date:',
+                          style: TextStyle(fontSize: 21),
+                        ),
                       ),
                     ),
                     Padding(padding: EdgeInsets.all(4.0)),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        SizedBox(
-                          width: 52,
-                          height: 75,
-                          child: TextField(
-                            maxLength: 2,
-                            controller: mController,
-                            textAlign: TextAlign.center,
-                            keyboardType: TextInputType.number,
-                            inputFormatters: [
-                              FilteringTextInputFormatter.digitsOnly,
-                            ],
-                            decoration: InputDecoration(
-                              border: OutlineInputBorder(),
-                              hintText: 'MM',
-                              counterText: "",
-                              contentPadding: EdgeInsets.symmetric(horizontal: 12),
+                    Expanded(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Container(
+                            height:MediaQuery.of(context).size.height/6.5,
+                            width: MediaQuery.of(context).size.width/6.5 ,
+                            child: TextField(
+                              maxLength: 2,
+                              controller: mController,
+                              textAlign: TextAlign.center,
+                              keyboardType: TextInputType.number,
+                              inputFormatters: [
+                                FilteringTextInputFormatter.digitsOnly,
+                              ],
+                              decoration: InputDecoration(
+                                border: OutlineInputBorder(),
+                                hintText: 'MM',
+                                counterText: "",
+                                contentPadding: EdgeInsets.symmetric(horizontal: 12),
+                              ),
                             ),
                           ),
-                        ),
-                        Padding(padding: EdgeInsets.all(7.0)),
-                        SizedBox(
-                          width: 52,
-                          height: 75,
-                          child: TextField(
-                            maxLength: 2,
-                            controller: dController,
-                            textAlign: TextAlign.center,
-                            keyboardType: TextInputType.number,
-                            inputFormatters: [
-                              FilteringTextInputFormatter.digitsOnly,
-                            ],
-                            decoration: InputDecoration(
-                              border: OutlineInputBorder(),
-                              hintText: 'DD',
-                              counterText: "",
-                              contentPadding: EdgeInsets.symmetric(horizontal: 12),
+                          Padding(padding: EdgeInsets.all(7.0)),
+                          Container(
+                            height:MediaQuery.of(context).size.height/6.5,
+                            width: MediaQuery.of(context).size.width/6.5 ,
+                            child: TextField(
+                              maxLength: 2,
+                              controller: dController,
+                              textAlign: TextAlign.center,
+                              keyboardType: TextInputType.number,
+                              inputFormatters: [
+                                FilteringTextInputFormatter.digitsOnly,
+                              ],
+                              decoration: InputDecoration(
+                                border: OutlineInputBorder(),
+                                hintText: 'DD',
+                                counterText: "",
+                                contentPadding: EdgeInsets.symmetric(horizontal: 12),
+                              ),
                             ),
                           ),
-                        ),
-                        Padding(padding: EdgeInsets.all(7.0)),
-                        SizedBox(
-                          width: 75,
-                          height: 75,
-                          child: TextField(
-                            maxLength: 4,
-                            controller: yrController,
-                            textAlign: TextAlign.center,
-                            keyboardType: TextInputType.number,
-                            inputFormatters: [
-                              FilteringTextInputFormatter.digitsOnly,
-                            ],
-                            decoration: InputDecoration(
-                              border: OutlineInputBorder(),
-                              alignLabelWithHint: true,
-                              hintText: 'YYYY',
-                              counterText: "",
-                              contentPadding: EdgeInsets.symmetric(horizontal: 12),
+                          Padding(padding: EdgeInsets.all(7.0)),
+                         Container(
+                            height:MediaQuery.of(context).size.height/6.5,
+                            width: MediaQuery.of(context).size.width/6.5 +25,
+                            child: TextField(
+                              maxLength: 4,
+                              controller: yrController,
+                              textAlign: TextAlign.center,
+                              keyboardType: TextInputType.number,
+                              inputFormatters: [
+                                FilteringTextInputFormatter.digitsOnly,
+                              ],
+                              decoration: InputDecoration(
+                                border: OutlineInputBorder(),
+                                alignLabelWithHint: true,
+                                hintText: 'YYYY',
+                                counterText: "",
+                                contentPadding: EdgeInsets.symmetric(horizontal: 12),
+                              ),
                             ),
                           ),
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
-                    Row(
-                      children: [
-                        Spacer(),
-                        ElevatedButton(
-                          child: const Text('Submit'),
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor: Colors.lightBlue.shade600,
-                            textStyle: TextStyle(fontSize: 17),
-                            fixedSize: Size(130, 43),
+                    Spacer(),
+                    Expanded(
+                      child: Row(
+                        children: [
+                          Spacer(),
+                          ElevatedButton(
+                            child: const Text('Submit'),
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.lightBlue.shade600,
+                              textStyle: TextStyle(fontSize: 17),
+                              fixedSize: Size(130, 43),
+                            ),
+                            onPressed: () {
+                              locationString = stateController.text + ' ' + countyController.text;
+                              location = stateController.text + ' ' + countyController.text;
+                              date = mController.text + '-' + dController.text + '-' + yrController.text;
+                              if (edit_file)
+                              {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(builder: (context) => EditFormsPage(false)),
+                                );
+                              }
+                              else if (view_pdf)
+                              {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(builder: (context) => EditFormsPage(true)),
+                                );
+                              }
+                              else
+                              {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(builder: (context) => FormsPage()),
+                                );
+                              }
+                            },
                           ),
-                          onPressed: () {
-                            locationString = stateController.text + ' ' + countyController.text;
-                            location = stateController.text + ' ' + countyController.text;                            
-                            date = mController.text + '-' + dController.text + '-' + yrController.text;
-                            if (edit_file)
-                            {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(builder: (context) => EditFormsPage(false)),
-                              );
-                            }
-                            else if (view_pdf)
-                            {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(builder: (context) => EditFormsPage(true)),
-                              );
-                            }
-                            else
-                            {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(builder: (context) => FormsPage()),
-                              );
-                            }
-                          },
-                        ),
-                        Spacer(),                  
-                      ],
+                          Spacer(),
+                        ],
+                      ),
                     ),
-                 
+                    Spacer(),
                   ],
                 ),
               ),
